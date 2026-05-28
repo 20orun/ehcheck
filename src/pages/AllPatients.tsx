c6fe49cec1d04eeebdb71077650441f524a1e961
Arun Krishnan V M <arunkrishnanvm@gmail.com>
2026-05-28 13:20:21 +0530

    Add files via upload

--- src/pages/AllPatients.tsx
+++ src/pages/AllPatients.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useApp } from '@/store/AppContext'
 import { CopyableUHID } from '@/components/CopyableUHID'
